bc0830d6246843822c08994a7bcedcd8ce467ab8
Terry Chan <chanchiho01@gmail.com>
2024-06-07 17:44:14 -0400

    feat: Toast notifications

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import { TextInput } from 'components/atoms/TextInput'
 import { AutoComplete } from 'components/molecules/AutoComplete'
 import { Options } from 'components/molecules/inputs/Options'
 import banner from 'public/banner.jpg';
+import { Bounce, ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 const MOUSE_ACTION_OPTIONS = [{ label: 'Single Click', value: 'singleClick' }, { label: 'Double Click', value: 'doubleClick' }]
 const MOUSE_BUTTON_TYPE_OPTIONS = [{ label: 'Middle Click', value: 'middle' }, { label: 'Left Click', value: 'left' }, { label: 'Right Click', value: 'right' }]
@@ -39,18 +41,27 @@ function App() {
 
 
   const onStop = useCallback(() => {
-    startedInterval && clearInterval(startedInterval)
+    if (startedInterval) {
+      clearInterval(startedInterval)
+      toast.info('Service Stopped');
+    }
     setStartedInterval(undefined)
   }, [startedInterval, setStartedInterval])
 
   const onStart = useCallback(() => {
+
+    if (!selectedProcesses.size && processSpecific) return toast.error('Please select a process to start.')
+
     setStartedInterval(setInterval(() => {
       const actionType = selectedMouseAction.value as "singleClick" | "doubleClick"
       const buttonType = selectedMouseButton.value as 'left' | "right" | "middle"
       window.mouseClick(actionType, buttonType)
 
     }, actionIntervalSecond * 1000))
-  }, [setStartedInterval, actionIntervalSecond, selectedMouseButton, selectedMouseAction])
+
+    toast.success('Service Started')
+
+  }, [setStartedInterval, actionIntervalSecond, selectedMouseButton, selectedMouseAction, processSpecific])
 
   const onSelectProcess = useCallback((option: {
     label: string;
@@ -79,7 +90,6 @@ function App() {
     }
 
     window.psList().then(setProcessList)
-
 
   }, [setSelectedProcesses, startedInterval, setProcessList, setProcessSpecific])
 
@@ -112,70 +122,87 @@ function App() {
 
   useEffect(() => {
     if (!processSpecific) return
-    const selectedProcessExist = [...selectedProcesses.values()].find(({ pid }) => processList.find(({ pid: originalPid }) => originalPid === pid))
-    if (!selectedProcessExist) {
+    const anySelectedProcessExist = !![...selectedProcesses.values()].find(({ pid }) => processList.find(({ pid: originalPid }) => originalPid === pid))
+    if (!anySelectedProcessExist) {
+      toast.info('Service stopped due to target process not found')
       onStop()
     }
   }, [processSpecific, processList, onStop, selectedProcesses])
 
   return (
-    <div className='flex h-screen flex-col'>
-      <img draggable={false} className='w-screen' src={banner} />
+    <>
+      <div className='flex h-screen flex-col'>
+        <img draggable={false} className='w-screen' src={banner} />
 
-      <div className='flex grow flex-col justify-between gap-2 p-3' style={{ flex: 1 }}>
+        <div className='flex grow flex-col justify-between gap-2 p-3' style={{ flex: 1 }}>
 
-        <div className='flex flex-col justify-between gap-2'>
+          <div className='flex flex-col justify-between gap-2'>
 
-          <FormContainer
-            onClearSection={onResetProcessStates}
-            clearButtonDisabled={!!startedInterval}
-            title='Application'
-            onToggleSwitch={() => setProcessSpecific(val => !val)}
-            showSwitch
-            switchValue={processSpecific}
-            toggleDisabled={!!startedInterval}
-          >
-            <div className='flex flex-col gap-4 py-2'>
-              <AutoComplete options={displayingProcessOptions} onSelect={onSelectProcess} onChange={setFilterKey} value={filterKey} />
-              {!!selectedProcesses.size && <div className='flex max-h-[4.5rem] grow-0 flex-wrap gap-2 overflow-y-scroll'>
-                {[...selectedProcesses.values()].map(({ name, pid }, idx) => <Tag key={`${name}-${pid}-${idx}`} onClick={() => onRemoveProcess(pid)} title={`${pid} - ${name}`} />)}
-              </div>}
-            </div>
-          </FormContainer >
-
-          <FormContainer title='Mouse Action'>
-            <div className='flex w-48 flex-col gap-4 py-1'>
-              <TextInput
-                description='Interval'
-                value={actionIntervalSecond}
-                onChange={(val) => setActionIntervalSecond(Number(val))}
-                type='number'
-                step={5}
-                suffix='s'
-                disabled={!!startedInterval}
-              />
-
-              <div className='flex w-48 flex-col gap-2'>
-                <span className='text-secondary'>Mouse Action</span>
-                <Options disabled={!!startedInterval} options={MOUSE_ACTION_OPTIONS} onSelect={onSelectMouseAction} selectedOption={selectedMouseAction} />
-                <Options disabled={!!startedInterval} options={MOUSE_BUTTON_TYPE_OPTIONS} onSelect={onSelectMouseButton} selectedOption={selectedMouseButton} />
+            <FormContainer
+              onClearSection={onResetProcessStates}
+              clearButtonDisabled={!!startedInterval}
+              title='Application'
+              onToggleSwitch={() => setProcessSpecific(val => !val)}
+              showSwitch
+              switchValue={processSpecific}
+              toggleDisabled={!!startedInterval}
+            >
+              <div className='flex flex-col gap-4 py-2'>
+                <AutoComplete options={displayingProcessOptions} onSelect={onSelectProcess} onChange={setFilterKey} value={filterKey} />
+                {!!selectedProcesses.size && <div className='flex max-h-[4.5rem] grow-0 flex-wrap gap-2 overflow-y-scroll'>
+                  {[...selectedProcesses.values()].map(({ name, pid }, idx) => <Tag key={`${name}-${pid}-${idx}`} onClick={() => onRemoveProcess(pid)} title={`${pid} - ${name}`} />)}
+                </div>}
               </div>
+            </FormContainer >
+
+            <FormContainer title='Mouse Action'>
+              <div className='flex w-48 flex-col gap-4 py-1'>
+                <TextInput
+                  description='Interval'
+                  value={actionIntervalSecond}
+                  onChange={(val) => setActionIntervalSecond(Number(val))}
+                  type='number'
+                  step={5}
+                  suffix='s'
+                  disabled={!!startedInterval}
+                />
+
+                <div className='flex w-48 flex-col gap-2'>
+                  <span className='text-secondary'>Mouse Action</span>
+                  <Options disabled={!!startedInterval} options={MOUSE_ACTION_OPTIONS} onSelect={onSelectMouseAction} selectedOption={selectedMouseAction} />
+                  <Options disabled={!!startedInterval} options={MOUSE_BUTTON_TYPE_OPTIONS} onSelect={onSelectMouseButton} selectedOption={selectedMouseButton} />
+                </div>
+              </div>
+            </FormContainer>
+
+          </div >
+
+          <div className='flex justify-between'>
+            <div className='space-x-4'>
+              <Button isDisabled={!!startedInterval} onClick={onStart}>Start</Button>
+              <Button isDisabled={!startedInterval} onClick={onStop}>Stop</Button>
             </div>
-          </FormContainer>
-
-        </div >
-
-        <div className='flex justify-between'>
-          <div className='space-x-4'>
-            <Button isDisabled={!!startedInterval} onClick={onStart}>Start</Button>
-            <Button isDisabled={!startedInterval} onClick={onStop}>Stop</Button>
+            <Button onClick={() => window.close()}>Exit</Button>
           </div>
-          <Button onClick={() => window.close()}>Exit</Button>
+
         </div>
-
       </div>
-    </div>
-
+      <ToastContainer
+        position="top-center"
+        autoClose={1500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Bounce}
+        toastStyle={{ margin: '12px 24px 24px' }}
+        toastClassName='bg-bg-alternative rounded-md border-b-0'
+        bodyClassName='font-semibold'
+      />
+    </>
   )
 }
 
